48e1a6d7c9a526ecd1768d43de1c029f5d7ef4b3
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2021-12-22 11:30:30 -0500

    testing

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include <string>
 #include <cstdlib>
 #include <vector>
+#include <ctime>
 #include "Motorcycle.hpp"
 
 int main() {
+    srand(time(NULL));
     Motorcycle bike;
 
     std::cout << bike.getBikeType() << std::endl;
